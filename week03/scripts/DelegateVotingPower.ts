1f22ff3f4a14072347d6635b7c64bb4e2389cf0b
Kate H. <106677924+kjhashim@users.noreply.github.com>
2024-11-11 19:50:38 +0000

    Update DelegateVotingPower.ts use sharedUtils

--- week03/scripts/DelegateVotingPower.ts
+++ week03/scripts/DelegateVotingPower.ts
@@ -1,46 +1,27 @@
 import {
-  createPublicClient,
-  createWalletClient,
   formatEther,
   parseEther,
   Hex,
   hexToString,
   http,
 } from "viem";
-import { sepolia } from "viem/chains";
-import * as dotenv from "dotenv";
-import { mnemonicToAccount, privateKeyToAccount } from "viem/accounts";
 import { abi, bytecode } from "../artifacts/contracts/MyERC20Votes.sol/MyToken.json";
-
-dotenv.config();
-
-const providerApiKey = process.env.ALCHEMY_API_KEY || "";
-const privateKey = process.env.PRIVATE_KEY || "";
+import { getClientsAccts } from "./sharedUtils";
 
 // contract address 0xE366C5a151e568eCBC46894E0791E8327b5310f8
-// npx ts-node --files ./scripts/DelegateVotingPower.ts 0xE366C5a151e568eCBC46894E0791E8327b5310f8 <# tokens to mint>
+// npx ts-node --files ./scripts/DelegateVotingPower.ts 0xE366C5a151e568eCBC46894E0791E8327b5310f8 <optional: address to delegate to if not self>
 async function main() {
     const parameters = process.argv.slice(2);
     if (!parameters || parameters.length < 1)
-        throw new Error("Required contract address parameter not provided. Required: <contract address> Optional: <wallet address>");
+        throw new Error("Required contract address parameter not provided. Required: <contract address> Optional: <address to delegate to if not self>");
     const contractAddress = parameters[0] as `0x${string}`;
     if (!contractAddress) throw new Error("Contract address not provided");
     if (!/^0x[a-fA-F0-9]{40}$/.test(contractAddress))
         throw new Error("Invalid contract address");
 
-    const publicClient = createPublicClient({
-        chain: sepolia,
-        transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
-    });
+    const { publicClient, account, deployer } = getClientsAccts();
     const blockNumber = await publicClient.getBlockNumber();
     console.log("Last block number: ", blockNumber);
-
-    const account = privateKeyToAccount(`0x${privateKey}`);
-    const deployer = createWalletClient({
-        account,
-        chain: sepolia,
-        transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
-    });
 
     let acctAddress : string;
     if (parameters.length >= 2) {
@@ -74,7 +55,7 @@ async function main() {
 
     console.log(
       `Account ${acctAddress}
-      has ${votes.toString()} units of voting power after self delegating\n`
+      has ${votes.toString()} units of voting power after delegating\n`
     );
 
 
